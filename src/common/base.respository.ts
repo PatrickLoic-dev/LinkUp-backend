558bc7f0145a682fb50590d7ae1c68df635dd041
Patrick Loic <kangueloic9@gmail.com>
2025-07-11 10:06:10 +0100

    refactor: update BaseRepository and AccountRepository to use Database abstraction and improve collection handling

--- src/common/base.respository.ts
+++ src/common/base.respository.ts
@@ -1,26 +1,28 @@
 // base.repository.ts
 import { NotFoundException, ServiceUnavailableException } from '@nestjs/common';
 import { Db, Collection, Document, WithId, OptionalUnlessRequiredId } from 'mongodb';
+import { Database } from 'src/database/database';
 
 export abstract class BaseRepository<T extends Document> {
-  protected collection: Collection<T>;
+    protected collectionName: string;
 
-  constructor(db: Db, entityClass: new () => T) {
-    const collectionName = entityClass.name.toLowerCase(); // Ex: "Utilisateur"
-    this.collection = db.collection<T>(collectionName);
+    constructor(protected database: Database) {
+        if (this.collectionName == null) {
+            this.collectionName = this.getCollectionName();
+        }
+    }
+
+  async findAll(): Promise<Document[]> {
+    return (await this.getDatabase()).collection(this.collectionName).find().toArray();
   }
 
-  async findAll(): Promise<WithId<T>[]> {
-    return this.collection.find().toArray();
+  async findById(id: string): Promise<WithId<Document> | null> {
+    return (await this.getDatabase()).collection(this.collectionName).findOne({ id } as any);
   }
 
-  async findById(id: string): Promise<WithId<T> | null> {
-    return this.collection.findOne({ id } as any);
-  }
+  async getBy(filter: Partial<T>, projection?: Record<string, any>): Promise<WithId<Document> | null> {
 
-  async getBy(filter: Partial<T>, projection?: Record<string, any>): Promise<WithId<T> | null> {
-
-    const data = await this.collection.findOne(filter as any, { projection });
+    const data = (await this.getDatabase()).collection(this.collectionName).findOne(filter as any, { projection });
 
     if (!data) {
       throw new NotFoundException('Resource not found');
@@ -30,7 +32,7 @@ export abstract class BaseRepository<T extends Document> {
   }
 
   async create(item: OptionalUnlessRequiredId<T>) {
-    const result = await this.collection.insertOne(item);
+    const result = (await this.getDatabase()).collection(this.collectionName).insertOne(item);
 
     if (!result) {
       throw new ServiceUnavailableException('Failed to create item');
@@ -40,13 +42,31 @@ export abstract class BaseRepository<T extends Document> {
     return { success: true, message: 'Operation successful' };
   }
 
-  async update(id: string, item: Partial<T>): Promise<WithId<T> | null> {
-    await this.collection.updateOne({ id } as any, { $set: item });
-    return this.findById(id);
+  async update(id: string, item: Partial<T>) {
+    const result = (await this.getDatabase()).collection(this.collectionName).updateOne({ id } as any, { $set: item });
+    return { success: true, message: 'Update successful' };
   }
 
   async delete(id: string): Promise<boolean> {
-    const result = await this.collection.deleteOne({ id } as any);
-    return result.deletedCount > 0;
+    const result = (await this.getDatabase()).collection(this.collectionName).deleteOne({ id } as any);
+    return (await result).deletedCount > 0;
   }
+
+  protected async getDatabase(): Promise<Db> {
+    const db = await this.database.getDatabase();
+    return db; 
+  } 
+
+  private getCollectionName(): string {
+    let name = this.constructor.name.replace('Collection', '').toLowerCase();
+
+    if (name.slice(-1) == 'y') {
+      name = name.replace('y', 'ies')
+      return name
+    }
+
+    return name + 's';
+  }
+
+
 }
--- src/modules/authentication/repository/account.repository.ts
+++ src/modules/authentication/repository/account.repository.ts
@@ -3,10 +3,11 @@ import { Db, WithId, OptionalUnlessRequiredId } from 'mongodb';
 import { BaseRepository } from '../../../common/base.respository';
 import { Account } from 'src/interfaces/Account';
 import * as bcrypt from 'bcrypt';
+import { Database } from 'src/database/database';
 
 export class AccountRepository extends BaseRepository<Account> {
-  constructor(db: Db) {
-    super(db, Account);
+  constructor(database: Database) {
+    super(database);
   }
 
   async createAccount(account: OptionalUnlessRequiredId<Account>){
@@ -19,6 +20,6 @@ export class AccountRepository extends BaseRepository<Account> {
   }
 
   async findByEmail(email: string): Promise<WithId<Account> | null> {
-    return this.collection.findOne({ email });
+    return super.getBy({ email }) as unknown as WithId<Account>;
   }
 }
